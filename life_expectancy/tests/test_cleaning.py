60258db333d5ecd2c34e3416829d78eca416b27c
timafonseca <tiago.m.fonseca@nos.pt>
2023-04-12 16:15:58 +0100

    Resolve conflits

--- life_expectancy/tests/test_cleaning.py
+++ life_expectancy/tests/test_cleaning.py
@@ -1,16 +1,29 @@
 """Tests for the cleaning module"""
-import pandas as pd
 
-from life_expectancy.cleaning import main
+from unittest.mock import patch
+import pandas as pd
+from life_expectancy.main import main
 from . import OUTPUT_DIR
 
-
-def test_clean_data(pt_life_expectancy_expected):
+@patch("life_expectancy.main.load_data")
+@patch("life_expectancy.main.save_data")
+def test_main(
+    mock_save, mock_load, pt_life_expectancy_expected, input_data
+):
     """Run the `clean_data` function and compare the output to the expected output"""
-    main("PT")
-    pt_life_expectancy_actual = pd.read_csv(
-        OUTPUT_DIR / "pt_life_expectancy.csv"
-    )
+
+    mock_load.return_value = input_data
+
+    mock_save.side_effect = print("saved", end="")
+
+    file_path = OUTPUT_DIR / "eu_life_expectancy_raw_test.tsv"
+
+    actual = main("PT").reset_index(drop=True)
+
+    mock_load.assert_called_once_with(file_path)
+
+    mock_save.assert_called_once()
+
     pd.testing.assert_frame_equal(
-        pt_life_expectancy_actual, pt_life_expectancy_expected
+        actual, pt_life_expectancy_expected
     )
